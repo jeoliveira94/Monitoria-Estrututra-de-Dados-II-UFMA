c6f2a5456a090a34f9364822d1577c7c1a00e1c5
José Eduardo <eduardo@gmail.com>
2019-03-29 07:49:34 -0300

    Removidos protoripos de algoritmos de ordenacão, inseridos discas de como deve ser implementado as comparações de arestas

--- algoritmosDeOrdenacao.py
+++ algoritmosDeOrdenacao.py
@@ -1,23 +1,19 @@
-#comparacoes
-#print(int(arestas[0]['weight']) > int(arestas[1]['weight']))
-#print(int(arestas[0]['weight']) < int(arestas[1]['weight']))
-#print(int(arestas[0]['weight']) == int(arestas[1]['weight']))
+'''
+Introdução:
+- Implementar algoritmo de ordenação que receba uma colecão
+- A coleção é uma lista de arestas
+- Para comparar o peso as arestas entre dois item da coleção basta usar a chave 'weight' (peso)
+
+Exemplos:
+- Modo convencional
+colecao[i] <operador de comparacao> colecao[j]
+colecao[i] <operador de comparacao> colecao[j]
+
+- Modo que você vai usar
+int(colecao[i]['weight']) <operador de comparacao> int(colecao[j]['weight'])
+
+É nescessário converter o valor pra Interger no momento da comparação afim de eviter erros
+'''
 
 def insertionSort(colecao):
-    for i in range(1, len(colecao)): 
-        key = colecao[i] 
-        j = i-1
-        while j >= 0 and key['weight'] < colecao[j]['weight']: 
-            colecao[j + 1] = colecao[j] 
-            j -= 1
-        colecao[j + 1] = key
     return
-
-def selecionSort(colecao): 
-    for i in range(len(colecao)): 
-        min_idx = i 
-        for j in range(i+1, len(colecao)): 
-            if int(colecao[min_idx]['weight']) > int(colecao[j]['weight']): 
-                min_idx = j
-        colecao[i], colecao[min_idx] = colecao[min_idx], colecao[i] 
-    return colecao
