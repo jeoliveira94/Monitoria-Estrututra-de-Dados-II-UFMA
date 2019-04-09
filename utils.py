30a8d5bbb48426fda472f45142e483ff084dc729
José Eduardo <eduardo.jeso@gmail.com>
2019-04-09 20:28:17 -0300

    arquivos movidos / deletados

--- utils.py
+++
@@ -1,4 +0,0 @@
-def SalvarArvoreGeradoraMinimaEmArquivo(arquivoDeSaida, arvoreGeradoraMinima):
-    with open(arquivoDeSaida, 'w') as arquivo:
-        for aresta in arvoreGeradoraMinima:
-            arquivo.write('fonte: {}, destino: {}, peso: {}\n'.format(aresta['source'], aresta['target'], aresta['weight']))
